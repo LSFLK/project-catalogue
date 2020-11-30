96eeef66d4c256ea908e5be8dd45d853f49420a8
Sennen Randika <randikasennen@gmail.com>
2020-11-30 09:47:03 +0530

    Created entities

---
+++ src/Repository/DomainExpertiseRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\DomainExpertise;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method DomainExpertise|null find($id, $lockMode = null, $lockVersion = null)
+ * @method DomainExpertise|null findOneBy(array $criteria, array $orderBy = null)
+ * @method DomainExpertise[]    findAll()
+ * @method DomainExpertise[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class DomainExpertiseRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, DomainExpertise::class);
+    }
+
+    // /**
+    //  * @return DomainExpertise[] Returns an array of DomainExpertise objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('d')
+            ->andWhere('d.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('d.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?DomainExpertise
+    {
+        return $this->createQueryBuilder('d')
+            ->andWhere('d.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
---
+++ src/Repository/TechnicalExpertiseRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\TechnicalExpertise;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method TechnicalExpertise|null find($id, $lockMode = null, $lockVersion = null)
+ * @method TechnicalExpertise|null findOneBy(array $criteria, array $orderBy = null)
+ * @method TechnicalExpertise[]    findAll()
+ * @method TechnicalExpertise[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TechnicalExpertiseRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, TechnicalExpertise::class);
+    }
+
+    // /**
+    //  * @return TechnicalExpertise[] Returns an array of TechnicalExpertise objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('t.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?TechnicalExpertise
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
