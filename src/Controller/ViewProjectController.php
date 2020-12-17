b1496ee6f8c4e206947358b9976ef2c948ff9848
Sennen Randika <randikasennen@gmail.com>
2020-12-17 17:27:14 +0530

    Updated view project page

--- src/Controller/ViewProjectController.php
+++ src/Controller/ViewProjectController.php
@@ -13,14 +13,19 @@ class ViewProjectController extends AbstractController
      */
     public function index(): Response
     {
+        $domain_expertise = str_replace(' ', '-', strtolower('Productivity Tools'));
+        $technical_expertise = str_replace(' ', '-', strtolower('Programming'));
+
         return $this->render('view_project/index.html.twig', [
             'name' => 'Ballerina',
-            'website' => 'ballerina.io',
             'objective' => 'An open source programming language and platform for cloud-era application programmers to easily write software that just works.',
             'description' => 'For decades, programming languages have treated networks simply as I/O sources. Ballerina introduces fundamental, new abstractions of client objects, services, resource functions, and listeners to bring networking into the language so that programmers can directly address the Fallacies of Distributed Computing as part of their application logic. This facilitates resilient, secure, performant network applications to be within every programmer’s reach.',
+            'organization' => 'Open Source Lanka',
+            'website' => 'ballerina.io',
+            'domain_expertise' => $domain_expertise,
+            'technical_expertise' => $technical_expertise,
+            'git_repositories' => 'https://github.com/ballerina-platform/ballerina-lang',
             'more_info' => 'Find blogs on Ballerina in community-driven Tech Blog.',
-            'repository' => 'https://github.com/ballerina-platform/ballerina-lang',
-            'categories' => array('programming'),
             'languages' => array('java', 'ballerina'),
             'tags' => array('programming-language', 'language', 'compiler')
         ]);
