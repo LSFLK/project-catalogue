db02db28b00db5759e89b8d80c74cecc2434c11a
Sennen Randika <randikasennen@gmail.com>
2020-12-22 14:37:32 +0530

    Modified tag box view

--- public/js/view_project/index.js
+++
@@ -1,6 +0,0 @@
-var infoTags = document.getElementsByClassName('info');
-
-for(index = 0; index < infoTags.length; index++) {
-    const info = infoTags[index].getElementsByTagName('a')[0].innerText;
-    infoTags[index].getElementsByTagName('a')[0].innerText = info.replace(/\s+/g, '-').toLowerCase();
-}
