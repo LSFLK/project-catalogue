143a40dfeb931cc13d0ae0b9942fa734134c116a
Sennen Randika <randikasennen@gmail.com>
2020-12-23 09:53:25 +0530

    Implemented GitHubAPI service

--- src/Service/GitHubAPI.php
+++ src/Service/GitHubAPI.php
@@ -2,26 +2,70 @@
 
 namespace App\Service;
 
-use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpClient\NativeHttpClient;
 
 class GitHubAPI
 {
-    private $client;
+    private $client, $gitHubInformation;
 
-    public function __construct(HttpClientInterface $client)
+    public function __construct($repoUrl)
     {
+        $client = new NativeHttpClient();
         $this->client = $client;
+        $this->fetchGitHubInformation($repoUrl);
     }
 
-    public function fetchGitHubInformation($repoUrl): array
+    private function _fetchContent($requestUrl): array
     {
-        $apiBaseURL = "https://api.github.com/repos";
-        $repoUrlPath = parse_url($repoUrl, PHP_URL_PATH);
-        $repoAPIURL = $apiBaseURL.$repoUrlPath;
-
-        $response = $this->client->request('GET', $repoAPIURL);
+        $response = $this->client->request('GET', $requestUrl, [
+            'headers' => ['Accept' => 'application/vnd.github.mercy-preview+json']
+        ]);
         $content = $response->toArray();
-
         return $content;
+    }
+
+    private function fetchGitHubInformation($repoUrl)
+    {
+        $apiUrl = "https://api.github.com/repos";
+        $repoUrlPath = parse_url($repoUrl, PHP_URL_PATH);
+        $repoApiUrl = $apiUrl.$repoUrlPath;
+        $content = $this->_fetchContent($repoApiUrl);
+        $this->gitHubInformation = $content;
+    }
+
+    public function getGitHubInformation(): array
+    {
+        return $this->gitHubInformation;
+    }
+
+    public function getLicenceName(): string
+    {
+        $licence = $this->gitHubInformation['licence'];
+
+        if($licence) { return $licence['name']; }
+        else { return null; }
+    }
+
+    public function getStarsCount()
+    {
+        return $this->gitHubInformation['stargazers_count'];
+    }
+
+    public function getForksCount()
+    {
+        return $this->gitHubInformation['forks_count'];
+    }
+
+    public function getLanguages(): array
+    {
+        $languagesUrl = $this->gitHubInformation['languages_url'];
+        $content = $this->_fetchContent($languagesUrl);
+        $languages = array_keys($content);
+        return $languages;   
+    }
+
+    public function getTopics(): array
+    {
+        return $this->gitHubInformation['topics'];
     }
 }
