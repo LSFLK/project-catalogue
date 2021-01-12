4974e8c954b469a9588cd66759c75cbc1b9a7d6d
Sennen Randika <randikasennen@gmail.com>
2021-01-12 10:42:59 +0530

    Added search results count.

--- public/js/projects/search.js
+++ public/js/projects/search.js
@@ -3,19 +3,25 @@ function initiallizeSearch () {
     const element = document.getElementById('search-by-name');
     const searchInput = element.querySelector(".search-box-input");
 
+    const searchResults = document.getElementById('search-results-count');
+    const searchResultsCount = searchResults.querySelector(".count");
+    const searchResultsDisplayText = searchResults.querySelector(".text");
+    const searchResultsSearchTerm = searchResults.querySelector(".search");
+
     const url = new URL(window.location.href);
     const { origin, pathname } = url;
     const browser_url = new URL(origin + pathname);
 
     var delayTimer;
+    var searchInputText;
 
     searchInput.addEventListener("keyup", (event) => {
-        const name = event.target.value.trim();
+        searchInputText = event.target.value.trim();
 
         if(delayTimer) { clearTimeout(delayTimer) }
 
-        if(name.length) {
-            browser_url.searchParams.set('name', name);
+        if(searchInputText.length) {
+            browser_url.searchParams.set('name', searchInputText);
         }
         else {
             searchInput.value = null;
@@ -50,5 +56,17 @@ function initiallizeSearch () {
         });
 
         $(container).fadeIn(200);
+
+        if(searchInputText.length) {
+            searchResultsCount.innerHTML = projects.length;
+            searchResultsDisplayText.innerHTML = '&nbsp;result' + (projects.length === 1 ? '&nbsp' : 's&nbsp') + 'for&nbsp';
+            searchResultsSearchTerm.innerHTML = searchInputText;
+        }
+        else {
+            searchResultsCount.innerHTML = null;
+            searchResultsDisplayText.innerHTML = null;
+            searchResultsSearchTerm.innerHTML = null;
+        }
+        
     }
 }
