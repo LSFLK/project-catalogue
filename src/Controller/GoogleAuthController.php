d291843218006fc40ba8220e8d64ae3f2ee313cb
Sennen Randika <randikasennen@gmail.com>
2021-01-13 14:33:47 +0530

    Configure authentication with Google.

--- src/Controller/GoogleAuthController.php
+++ src/Controller/GoogleAuthController.php
@@ -3,43 +3,28 @@
 namespace App\Controller;
 
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
-use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class GoogleAuthController extends AbstractController
 {
     /**
-     * Link to this controller to start the "connect" process
-     * @param ClientRegistry $clientRegistry
-     *
      * @Route("/signin/google", name="google_signin")
-     *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
-    public function connectAction(ClientRegistry $clientRegistry)
+    public function connectAction(ClientRegistry $clientRegistry) : RedirectResponse
     {
         return $clientRegistry
             ->getClient('google')
-            ->redirect([
-                'profile', 'email' // the scopes you want to access
-            ])
+            ->redirect(['profile', 'email'])
         ;
     }
 
     /**
-     * After going to Google, you're redirected back here
-     * because this is the "redirect_route" you configured
-     * in config/packages/knpu_oauth2_client.yaml
-     *
-     * @param Request $request
-     * @param ClientRegistry $clientRegistry
-     *
-     * @Route("/auth/google", name="google_auth")
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/signin/google/auth", name="google_auth")
      */
-    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
+    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry) : RedirectResponse
     {
         return $this->redirectToRoute('projects');
     }
