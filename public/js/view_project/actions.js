940ac4077109a919e03e105c143ec72fe76a1daa
Sennen Randika <randikasennen@gmail.com>
2021-01-19 12:28:56 +0530

    Add actions bar.

---
+++ public/js/view_project/actions.js
@@ -0,0 +1,13 @@
+function copyLinkToClipboard() {
+    const button = document.getElementById("copy_link");
+    const link = button.getAttribute('link');
+    const element = document.createElement('input');
+
+    element.value = link;
+    document.body.appendChild(element);
+    element.select();
+    document.execCommand('copy');
+    document.body.removeChild(element);
+
+    alert("Project link has been copied to clipboard.");
+}
