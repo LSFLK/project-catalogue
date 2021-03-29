8dfe467a22f830e38bef6f6033b95c1a7cd49644
Sennen Randika <randikasennen@gmail.com>
2021-03-29 17:15:11 +0530

    Fix issue

--- src/Controller/RegisterOrganizationController.php
+++ src/Controller/RegisterOrganizationController.php
@@ -6,6 +6,7 @@ use App\Service\OrganizationHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @IsGranted("IS_AUTHENTICATED_FULLY")
