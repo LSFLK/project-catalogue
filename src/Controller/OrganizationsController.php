628ba67a83d933409753543850687bb0c08b5a73
Sennen Randika <randikasennen@gmail.com>
2021-03-29 17:20:01 +0530

    Create Organizations Controller

---
+++ src/Controller/OrganizationsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrganizationsController extends AbstractController
+{
+    /**
+     * @Route("/organizations", name="organizations")
+     */
+    public function index(): Response
+    {
+        return $this->render('organizations/index.html.twig', [
+            'controller_name' => 'OrganizationsController',
+        ]);
+    }
+}
