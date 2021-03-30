2c4d4be3f6a74fb8749cab7fab5ab7d6dcc57316
Sennen Randika <randikasennen@gmail.com>
2021-03-30 08:00:30 +0530

    Fix issues

--- src/Controller/OrganizationsController.php
+++ src/Controller/OrganizationsController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Organization;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrganizationsController extends AbstractController
--- src/Controller/RegisterOrganizationController.php
+++ src/Controller/RegisterOrganizationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Service\OrganizationHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -47,7 +48,7 @@ class RegisterOrganizationController extends AbstractController
     /**
      * @Route("/register/organization/success", name="register_organization_success")
      */
-    public function success(Request $request, SessionInterface $session): Response
+    public function success(Request $request): Response
     {
         $organization_token = $request->query->get('token');
         $organization_id = $request->query->get('id');
