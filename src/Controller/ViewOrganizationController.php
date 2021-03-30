88fff460fbc64aeb9933b15390bc133d946923fe
Sennen Randika <randikasennen@gmail.com>
2021-03-30 15:40:40 +0530

    Fix issue

--- src/Controller/ViewOrganizationController.php
+++ src/Controller/ViewOrganizationController.php
@@ -12,12 +12,12 @@ class ViewOrganizationController extends AbstractController
     /**
      * @Route("/organizations/id={id}", name="view_organization")
      */
-    public function index(): Response
+    public function index($id): Response
     {
         $organization = $this->getDoctrine()->getRepository(Organization::class)->find($id);
         $is_owner = $organization->getOwner() === $this->getUser();
         
-        return $this->render('view_project/index.html.twig', [
+        return $this->render('view_organization/index.html.twig', [
             'organization' => $organization,
             'is_owner' => $is_owner,
             'dir' => $this->getParameter('public_confirmed_dir')
