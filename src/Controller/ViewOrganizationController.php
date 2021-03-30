939f72b8c192b9b81dd32dd78f2e7a13f1b6d7bf
Sennen Randika <randikasennen@gmail.com>
2021-03-30 15:38:42 +0530

    Create View Organization Controller.

---
+++ src/Controller/ViewOrganizationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Organization;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ViewOrganizationController extends AbstractController
+{
+    /**
+     * @Route("/organizations/id={id}", name="view_organization")
+     */
+    public function index(): Response
+    {
+        $organization = $this->getDoctrine()->getRepository(Organization::class)->find($id);
+        $is_owner = $organization->getOwner() === $this->getUser();
+        
+        return $this->render('view_project/index.html.twig', [
+            'organization' => $organization,
+            'is_owner' => $is_owner,
+            'dir' => $this->getParameter('public_confirmed_dir')
+        ]);
+    }
+}
